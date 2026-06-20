e769bab7c611d805188263ba273ccadf2310ed9e
Andrew Cue <andrewcue@outlook.com>
2026-06-19 20:30:46 -0500

    board initialization

--- frontend/src/script.js
+++ frontend/src/script.js
@@ -1,3 +1,9 @@
-import { Chess } from 'chess.js'
+import { Chess } from 'https://cdn.jsdelivr.net/npm/chess.js@1.0.0-beta.6/+esm';
 
-const chess = new Chess()
+const config = {
+  position: 'start',
+  pieceTheme: 'https://chessboardjs.com/img/chesspieces/wikipedia/{piece}.png'
+};
+
+const chess = new Chess();
+var board = Chessboard('myBoard', config);
